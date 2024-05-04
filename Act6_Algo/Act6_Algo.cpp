84336b2a0ed1d6757e9e8cc94452eab2c37def2a
sekarasriii <sekareeesh@gmail.com>
2024-05-04 23:26:57 +0700

    perintah untuk membuat node baru di awal

--- Act6_Algo/Act6_Algo.cpp
+++ Act6_Algo/Act6_Algo.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
+struct Node
+{
 	int noMhs;
 	string name;
 	Node* next;
 	Node* prev;
 };
+
 Node* START = NULL;
 
-void addNode() {
+void addNode()
+{
 	Node* newNode = new Node(); // step 1: create a new node
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs; // Assign value to the data field of the new node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; // Assign value to the data field of the new node
 
+	// Insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{
+		// step 2 : insert the new node at the beginning
+
+	}
 }
