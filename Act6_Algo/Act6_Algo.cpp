29e98fef8aae5b8e91ee077c3bc791aba3299bdb
sekarasriii <sekareeesh@gmail.com>
2024-05-04 23:29:03 +0700

    perintah untuk list yang kosong

--- Act6_Algo/Act6_Algo.cpp
+++ Act6_Algo/Act6_Algo.cpp
@@ -23,6 +23,14 @@ void addNode()
 	if (START == NULL || newNode->noMhs <= START->noMhs)
 	{
 		// step 2 : insert the new node at the beginning
+		if (START == NULL || newNode->noMhs <= START->noMhs)
+		{ // step 2 : insert the new node at the beginning
+			if (START != NULL && newNode->noMhs == START->noMhs)
+			{
+				cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+				return;
+			}
+			// If the list is empty, make the new node the START
 
 	}
 }
