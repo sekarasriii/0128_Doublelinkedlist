61ab8200c929e213ebdc7422c0506e72e8350668
sekarasriii <sekareeesh@gmail.com>
2024-05-04 23:35:35 +0700

    masukkan fungsi search dengan tipe data boolean dan deklarasi variable roll nomor, pointer previous dan current

--- Act6_Algo/Act6_Algo.cpp
+++ Act6_Algo/Act6_Algo.cpp
@@ -42,7 +42,35 @@ void addNode()
 		else
 		{
 			// Insert the new node in the middle or at the end
+			Node * current = START; // step 1.a : start from the first node
+			Node* previous = NULL; // step 1.b : previous node is NULL initially
 
+			while (current != NULL && current->noMhs < newNode->noMhs)
+			{						// step 1.c : traverse the list to find the correct position
+				previous = current; // step 1.d : move the previous to the current node
+				current = current->next; // step 1.e : move the current to the next node
+			}
+			newNode->next = current; // step 4 : Make the next field of the new node point to current
+			newNode->prev = previous; // step 5 : Make the previous field of the new node point to previous
+
+			if (current != NULL)
+			{
+				current->prev = newNode; // step 6 : Make the previous field of the current node point to the new node
+
+			}
+			if (previous != NULL)
+			{
+				previous->next = newNode; // step 7 Make the next field of the previous node point to the new node
+			}
+			else
+			{
+				// If previous is still NULL, it means newNode is now the first node
+				START = newNode;
+			}
 		}
 	}
+}
+
+bool search(int rollNo, Node* previous, Node* current) {
+
 }
